794178d10896a67e3150c2e1147d7062f68cd532
amankhatri <kaman@uab.edu>
2014-07-26 15:14:48 -0500

    Ex 16 Extra Credit, C learn the hardway

---
+++ ex16ExtraCredit.c
@@ -0,0 +1,38 @@
+/*this program is extra credit for EX16 learn c the hardway*/
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<assert.h>
+
+struct Person
+{
+	char *name;
+	int age;
+	int height;
+	int weight;
+}person;
+void Person_create(char *name, int age, int height, int weight)
+{
+	person.name = name;
+	person.age = age;
+	person.height = height;
+	person.weight = weight;
+	//return person;
+}
+void Person_print()
+{
+	printf("Name: %s\n",person.name);
+	printf("Age: %d\n", person.age);
+	printf("Height: %d\n", person.height);
+	printf("Weight: %d\n",person.weight);
+}
+	
+
+int main(int argc, char *argv[])
+{
+	Person_create("Joe Alex",32,64,140);
+	Person_print();
+	Person_create("Frank Blank",20,72,180);
+	Person_print();
+	return 0;
+}
